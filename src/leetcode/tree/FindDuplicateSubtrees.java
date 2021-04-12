1605e1ac14e0dd449e95f97b7f7345ad3c5fbea5
qiuhonglong <742686556@qq.com>
2021-04-12 21:03:38 +0800

    add

---
+++ src/leetcode/tree/FindDuplicateSubtrees.java
@@ -0,0 +1,53 @@
+package leetcode.tree;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一棵二叉树，返回所有重复的子树。对于同一类的重复子树，你只需要返回其中任意一棵的根结点即可
+ * 两棵树重复是指它们具有相同的结构以及相同的结点值
+ *
+ * @author QiuHongLong
+ */
+public class FindDuplicateSubtrees {
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        doFindDuplicateSubtrees(root);
+        return nodes;
+    }
+
+    private HashMap<String, Integer> map = new HashMap<>();
+    private List<TreeNode> nodes = new LinkedList<>();
+
+    /**
+     * 用字符串标识树的唯一性
+     */
+    private String doFindDuplicateSubtrees(TreeNode node) {
+        if (node == null) return "#";
+        String left = doFindDuplicateSubtrees(node.left);
+        String right = doFindDuplicateSubtrees(node.right);
+        String curr = Integer.toString(node.val);
+        String path = left + "," + curr + "," + right;
+        if (map.containsKey(path)) {
+            if (map.get(path) == 1) nodes.add(node);
+            map.put(path, map.get(path) + 1);
+        } else {
+            map.put(path, 1);
+        }
+        return path;
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(2);
+        TreeNode node4 = new TreeNode(4);
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        new FindDuplicateSubtrees().findDuplicateSubtrees(node1);
+    }
+
+}
--- src/leetcode/tree/frame.md
+++ src/leetcode/tree/frame.md
@@ -1,10 +1,26 @@
 ``` java
 void traverse(TreeNode root) {
     // 前序遍历
-    traverse(root.left)    // left
+    traverse(root.left)    // root.left
     // 中序遍历 
-    traverse(root.right)   // right
+    traverse(root.right)   // root.right
     // 后序遍历
+}
+```
+
+``` java
+private int index = 0;
+void buildTree(int[] nums) {
+    int num = nums[index];
+    TreeNode node = new TreeNode(num)
+    if (num == 0) {
+        return null;
+    } else {
+        node.value = num;
+        node.left = buildTree(nums, index++);
+        node.right = buildTree(nums, index++);
+        return node;
+    }
 }
 ```
 
