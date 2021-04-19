dd886baa878babb501af5682586786ede2c31b05
qiuhonglong <742686556@qq.com>
2021-04-19 20:19:00 +0800

    add

---
+++ src/leetcode/bfs/MinDepthOfBTree.java
@@ -0,0 +1,52 @@
+package leetcode.bfs;
+
+import leetcode.tree.TreeNode;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class MinDepthOfBTree {
+
+
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        Set<TreeNode> set = new HashSet<>();
+        queue.add(root);
+        set.add(root);
+        int step = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                TreeNode left = node.left;
+                TreeNode right = node.right;
+                if (left == null && right == null) return step;
+                if (left != null && !set.contains(left)) {
+                    queue.add(left);
+                    set.add(left);
+                }
+                if (right != null && !set.contains(right)) {
+                    queue.add(right);
+                    set.add(right);
+                }
+            }
+            step++;
+        }
+        return step;
+    }
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        n1.left = n2;
+        n2.left = n3;
+        System.out.println(new MinDepthOfBTree().minDepth(n1));
+    }
+
+
+
+}
---
+++ src/leetcode/binsearch/frame.md
@@ -0,0 +1,50 @@
+#### 寻找一个数
+
+```java
+//  ∵ int right = nums.length - 1; 
+//  ∴ 搜索范围是 [left, right]，闭区间
+//  ∴ [left + 1, left] 为空
+//  ∴ while (left <= right)  
+int binarySearch(int[] nums, int target) {
+	int left = 0;
+    int right = nums.length - 1; // 注意
+	while (left <= right) {
+		int mid = left + (right - left) / 2;  // 固定的
+		if (nums[mid] == target)   // if elseif elseif 写全情况
+			return mid;		// 注意
+		else if (nums[mid] < target)
+			left = mid + 1; // 注意 + 1  (因为是闭区间)
+		else if (nums[mid] > target)
+			right = mid - 1; // 注意 - 1 (因为是闭区间)
+	} 
+    return -1;
+}
+```
+
+#### 寻找左侧边界
+
+```java
+int left_bound(int[] nums, int target) {
+	int left = 0, right = nums.length - 1;
+	// 搜索区间为 [left, right]
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+		if (nums[mid] == target) {
+            // 继续收缩右侧边界 [left, mid-1]
+			right = mid - 1;
+		} else if (nums[mid] > target) {
+			// 搜索区间变为 [left, mid-1]
+			right = mid - 1;
+		} else if (nums[mid] < target) {
+			// 搜索区间变为 [mid+1, right]
+			left = mid + 1;
+		}
+	} 
+    // 检查出界情况
+	if (left >= nums.length || nums[left] != target)
+		return -1;
+	return left;
+}
+```
+
+#### 
