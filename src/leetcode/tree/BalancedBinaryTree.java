1b7794a1fd70c36faf8a2725a95ae5ff5fc0dc28
qiuhonglong <742686556@qq.com>
2021-11-19 18:38:17 +0800

    modify

---
+++ src/leetcode/tree/BalancedBinaryTree.java
@@ -0,0 +1,21 @@
+package leetcode.tree;
+
+public class BalancedBinaryTree {
+
+    private boolean isBalanced = true;
+
+    public boolean isBalanced(TreeNode root) {
+        findDepth(root);
+        return isBalanced;
+    }
+
+    public int findDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = findDepth(root.left);
+        int right = findDepth(root.right);
+        if (Math.abs(right-left)> 1) isBalanced = false;
+        return Math.max(left, right) + 1;
+    }
+
+
+}
