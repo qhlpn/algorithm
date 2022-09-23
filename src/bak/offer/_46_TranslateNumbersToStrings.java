e2a1b5ba63e9e30d545a84a1f013fb58e1a97ece
qiuhonglong <742686556@qq.com>
2022-09-23 16:49:18 +0800

    modify

--- src/bak/offer/_46_TranslateNumbersToStrings.java
+++ src/bak/offer/_46_TranslateNumbersToStrings.java
@@ -3,8 +3,10 @@ package bak.offer;
 
 import java.util.Stack;
 
-// 将数字翻译成字符串 0 -- >a  1 --> b  ...  11 --> l ... 25 --> z
-// 实现一个函数，打印出一个数字的所有翻译方法
+/**
+ * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。
+ * 一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+ */
 public class _46_TranslateNumbersToStrings {
 
     public static void translateNumbersToStrings(int number) {
